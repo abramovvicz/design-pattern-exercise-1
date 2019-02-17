968f28c4b69be7fc9824def431e8bd8f85b49c95
abramovvicz <abramovvicz@gmail.com>
2019-02-17 12:05:01 +0100

    some small changes

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,7 +8,9 @@ public class Main {
     public static void main(String[] args) {
         BarChart barChart = new BarChart("Task types report", new ArrayList());
 
-        BarChart barChartBuilder = new BarChart.BarChartBuilder("some title", 800, 600)
+        BarChart barChart2 = new BarChart.BarChartBuilder("some title", 800, 600)
                 .withBackgroundColor("FFF000").withFontColor("white").withFontName("Arial").withFontSize(12).build();
+
+        
     }
 }
