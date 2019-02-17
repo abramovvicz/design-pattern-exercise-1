3edf1a187ba882ac92d57ef9639dc533d3cbd29f
abramovvicz <abramovvicz@gmail.com>
2019-02-17 12:02:59 +0100

    add Fluent Builder for BarChart

--- src/main/java/chart/BarChart.java
+++ src/main/java/chart/BarChart.java
@@ -33,6 +33,10 @@ public class BarChart {
         this.displayLegend = displayLegend;
     }
 
+    public BarChart() {
+
+    }
+
     public BarChart(String title, List<String> data) {
         this.title = title;
         this.data = data;
@@ -125,4 +129,83 @@ public class BarChart {
     public void setDisplayLegend(boolean displayLegend) {
         this.displayLegend = displayLegend;
     }
+
+
+    public static class BarChartBuilder {
+        private String title;
+
+        private String fontName;
+        private int fontSize;
+        private String fontColor;
+
+        private int width;
+        private int height;
+        private String backgroundColor;
+
+        private List<String> data;
+
+        private boolean displayTitle;
+        private boolean displayLegend;
+
+        public BarChartBuilder(String title, int width, int height) {
+            this.title = title;
+            this.width = width;
+            this.height = height;
+
+        }
+
+
+        public BarChartBuilder withFontName(String fontName) {
+            this.fontName = fontName;
+            return this;
+        }
+
+        public BarChartBuilder withFontSize(int fontSize) {
+            this.fontSize = fontSize;
+            return this;
+        }
+
+        public BarChartBuilder withFontColor(String fontColor) {
+            this.fontColor = fontColor;
+            return this;
+        }
+
+
+        public BarChartBuilder withBackgroundColor(String backgroundColor) {
+            this.backgroundColor = backgroundColor;
+            return this;
+        }
+
+        public BarChartBuilder withData(List<String> data) {
+            this.data = data;
+            return this;
+        }
+
+        public BarChartBuilder withDisplayTitle(boolean displayTitle) {
+            this.displayTitle = displayTitle;
+            return this;
+        }
+
+        public BarChartBuilder withDisplayLegend(boolean displayLegend) {
+            this.displayLegend = displayLegend;
+            return this;
+        }
+
+
+        public BarChart build() {
+            BarChart barChart = new BarChart();
+            barChart.setTitle(this.title);
+            barChart.setData(this.data);
+            barChart.setBackgroundColor(this.backgroundColor);
+            barChart.setDisplayLegend(this.displayLegend);
+            barChart.setFontColor(this.fontColor);
+            barChart.setFontName(this.fontName);
+            barChart.setHeight(this.height);
+            barChart.setWidth(this.width);
+            barChart.setDisplayTitle(this.displayTitle);
+            barChart.setFontSize(this.fontSize);
+            return barChart;
+        }
+    }
+
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        BarChart barChart = new BarChart("Task types report",new ArrayList());
+        BarChart barChart = new BarChart("Task types report", new ArrayList());
+
+        BarChart barChartBuilder = new BarChart.BarChartBuilder("some title", 800, 600)
+                .withBackgroundColor("FFF000").withFontColor("white").withFontName("Arial").withFontSize(12).build();
     }
 }
